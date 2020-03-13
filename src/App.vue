66f1e0233142346d576f67a79b8e687a2b8f1de2
Joshua Friedman <4homeworkstuff@gmail.com>
2020-03-12 18:15:59 -0700

    file selector if no path

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
     </div>
     <div v-else>
       <RecipeSearch />
-      No Path
+      <input type="file" @click="setNull" @change="updateFile" />
     </div>
   </div>
 </template>
@@ -14,31 +14,40 @@
 import Recipe from "./components/Recipe.vue";
 import RecipeSearch from "./components/RecipeSearch.vue";
 export default {
-  name: 'App',
+  name: "App",
   components: {
     Recipe,
     RecipeSearch
   },
-  data: function(){
-    return{
+  data: function() {
+    return {
       is_path: false,
       path: ""
+    };
+  },
+  beforeMount() {
+    var electron = require("electron");
+    var currentWindow = electron.remote.getCurrentWindow();
+    window.console.log(currentWindow.custom);
+    var path = currentWindow.custom;
+    window.console.log(electron.remote.process);
+    //  path = "/Users/Joshua/Desktop/Chcolate chip cookies copy.JSHN.JSON"// DELETE BEFORE BUILD!!
+    // check if file is opened
+    if (path != "") {
+      this.is_path = true;
+      this.path = path;
     }
   },
-  beforeMount(){
-    var electron = require('electron');
-var currentWindow = electron.remote.getCurrentWindow();
-window.console.log(currentWindow.custom);
-var path = currentWindow.custom;
-window.console.log(electron.remote.process);
-//  path = "/Users/Joshua/Desktop/Chcolate chip cookies copy.JSHN.JSON"// DELETE BEFORE BUILD!!
-    // check if file is opened
-  if(path!=""){
-    this.is_path = true;
-    this.path = path;
+  methods:{
+    setNull: function(event){
+      event.target.value = null;
+    },
+    updateFile: function(event){
+      this.path = event.target.files[0].path;
+      this.is_path = true;
+    }
   }
-  }
-}
+};
 </script>
 
 <style>
