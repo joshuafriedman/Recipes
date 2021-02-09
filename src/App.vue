be6481773a54abf8ace6c4fcd66119c4e463f842
Joshua Friedman <4homeworkstuff@gmail.com>
2021-02-09 01:02:49 -0800

    rm update recipe button (using git commands)

--- src/App.vue
+++ src/App.vue
@@ -27,19 +27,6 @@
         <div>
           <button @click="createRecipe" class="create-recipe">Create new recipe</button>
         </div>
-        <div>
-          <button @click="getUpdate" class="create-recipe">Get Latest Updates</button>
-          <br />
-          <br />
-          <br />
-          <div id="updating" style="display:none; white-space: pre-line;">
-            Getting the latest update... please wait <span id="wait"></span>
-          </div>
-          <p
-            id="update-message"
-            style="white-space: pre-line; text-align:left; width:500px; margin-left:auto; margin-right:auto;"
-          ></p>
-        </div>
       </div>
       <br />
       <RecipeSearch v-on:openRecipe="openTheRecipe" />
@@ -52,7 +39,6 @@ import Recipe from "./components/Recipe.vue";
 import RecipeSearch from "./components/RecipeSearch.vue";
 import CreateRecipe from "./components/CreateRecipe";
 import EditRecipe from "./components/EditRecipe";
-import the_big_string from "./assets/bashString.js";
 
 export default {
   name: "App",
@@ -132,87 +118,6 @@ export default {
     },
     goBackToRecipe: function() {
       this.edit_recipe = false;
-    },
-    getUpdate: function() {
-      document.getElementById("updating").innerHTML =
-        "Getting the latest update... please wait";
-      const fs = require("fs");
-      setTimeout(() => {
-        fs.readFile("latestUpdate.txt", (err, data) => {
-          if (err) throw err;
-          data = data.toString();
-          data = data.slice(data.indexOf("$") + 1, data.length - 1);
-          document.getElementById("update-message").innerHTML = data;
-        });
-      }, 3000);
-
-      document.getElementById("updating").style.display = "block";
-
-      var path = require("path");
-      var nwDir = path.dirname(process.execPath);
-      window.console.log(process);
-      window.console.log(nwDir);
-      // const cmd = "cd && cd desktop/project/frame && chmod u+x update.sh && ./update.sh";
-      const cmd = the_big_string.the_big_string;
-      // window.console.log(cmd);
-      const { exec } = require("child_process");
-
-      exec(cmd, (err, stdout, stderr) => {
-        if (err) {
-          // node couldn't execute the command
-          window.console.log("error has occured!!!!!");
-          window.console.log(err);
-          err = err.message;
-          if (
-            err.indexOf(
-              "error: Your local changes to the following files would be overwritten by merge"
-            ) != -1
-          ) {
-            document.getElementById("update-message").style = "display:none;";
-            const merge_files = err.substr(
-              err.indexOf(
-                "error: Your local changes to the following files would be overwritten by merge"
-              ) + 78,
-              err.indexOf("Please commit your changes or stas") - 129
-            );
-            window.console.log("adsf " + merge_files);
-            document.getElementById("updating").innerHTML =
-              "There is a merge conflict with " +
-              merge_files +
-              " \n please contact josh";
-          } else {
-            document.getElementById("updating").innerHTML =
-              "A small error has occured, talk to josh";
-          }
-          return;
-        }
-        document.getElementById("updating").innerHTML =
-          "Update Complete! Close and re-open the app for the latest update";
-        // the *entire* stdout and stderr (buffered)
-        window.console.log(`stdout: ${stdout}`);
-        window.console.log(`stderr: ${stderr}`);
-        if (stdout.indexOf("already have the latest") != -1) {
-          document.getElementById("update-message").style = "display:none;";
-          document.getElementById("updating").innerHTML =
-            "You already have the latest updates";
-        } else if (
-          stdout.indexOf(
-            "error: Your local changes to the following files would be overwritten by merge"
-          ) != -1
-        ) {
-          document.getElementById("update-message").style = "display:none;";
-          const merge_files = stdout.substr(
-            stdout.indexOf(
-              "error: Your local changes to the following files would be overwritten by merge"
-            ),
-            stdout.indexOf("Please commit your changes or stas")
-          );
-          document.getElementById("updating").innerHTML =
-            "There is a merge conflict with " +
-            merge_files +
-            "\n please contact josh";
-        }
-      });
     },
     changeTitle: function(new_title) {
       window.console.log(document.getElementsByTagName("title"));
